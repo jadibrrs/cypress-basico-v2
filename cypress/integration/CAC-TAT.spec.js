be56086fbb5bb601e4d4d23fc16860ec3bdf5eed
Jadi Barros Heliodoro <jbheliodoro@gmail.com>
2023-11-21 11:00:34 -0400

    após aprender a validar campo vazio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -22,12 +22,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
-        cy.get('input[id="firstName"]').type('Jadi')
-        cy.get('input[id="lastName"]').type('Barros')
-        cy.get('input[id="email"]').type('jbheliodoro')
-        cy.get('textarea[id="open-text-area"]').type('Teste', {delay: 0 })
+        cy.get('#firstName').type('Jadi')
+        cy.get('#lastName').type('Barros')
+        cy.get('#email').type('jbheliodoro')
+        cy.get('#open-text-area').type('Teste', {delay: 0 })
         cy.get('button[type="submit"]').click()
 
-        cy.get('span[class="error"]').should('be.visible')
+        cy.get('.error').should('be.visible')
+    })
+
+    it('Campo telefone continua vazio quando preenchido com valor não numérico', function() {
+        cy.get('#phone')
+            .type('abcdefghij')
+            .should('have.value', '')
     })
 })
