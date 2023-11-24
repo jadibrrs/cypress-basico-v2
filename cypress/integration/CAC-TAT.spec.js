607f5ec8be36e6a1b8196fe0cac4bb32be9f263e
Jadi Barros Heliodoro <jbheliodoro@gmail.com>
2023-11-23 22:19:36 -0400

    após aprender a selecionar opção dentro de elemento select

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -36,4 +36,68 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .type('abcdefghij')
             .should('have.value', '')
     })
+
+    it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('#firstName').type('Jadi')
+        cy.get('#lastName').type('Barros')
+        cy.get('#email').type('jbheliodoro@gmail.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('Preenche e limpa os campos nome, sobrenome, email e telefone + Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.get('input[id="firstName"]')
+            .type('Jadi')
+            .should('have.value', 'Jadi')
+            .clear()
+            .should('have.value', '')
+
+        cy.get('input[id="lastName"]')
+            .type('Barros')
+            .should('have.value', 'Barros')
+            .clear()
+            .should('have.value', '')
+
+        cy.get('input[id="email"]')
+            .type('jbheliodoro@gmail.com')
+            .should('have.value', 'jbheliodoro@gmail.com')
+            .clear()
+            .should('have.value', '')
+
+        cy.get('textarea[id="open-text-area"]')
+            .type('Teste')
+            .should('have.value', 'Teste')
+            .clear()
+            .should('have.value', '')
+
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('Envia o formulário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSubmit()
+
+        cy.get('.success').should('be.visible')
+    })
+
+    it('Seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('Seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it.only('Seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
 })
